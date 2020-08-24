4d6b8f8b37aaadc85e3c56bc1969e88689a83da3
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2020-08-24 22:04:40 +0530

    added material-ui icons

--- gatsby-boilerplate/gatsby-config.js
+++ gatsby-boilerplate/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-material-ui`,
+    `gatsby-theme-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
